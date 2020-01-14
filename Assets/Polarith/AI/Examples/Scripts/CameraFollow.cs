60aa0d14cc02dd2e082e31c54d005000d25359c7
Martin Zettwitz <martin.zettwitz@polarith.com>
2020-01-14 18:01:52 +0100

    added battlefields scene and new dll

---
+++ Assets/Polarith/AI/Examples/Scripts/CameraFollow.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+
+namespace Polarith.AI.Package
+{
+    /// <summary>
+    /// A script that is used to let a <see cref="Camera"/> follow a <see cref="Target"/> object. You can adjust <see
+    /// cref="Offset"/>, <see cref="MoveSpeed"/>, and <see cref="CameraAngle"/>, whereas CameraAngle only affects the
+    /// X-value of the camera´s rotation. The <see cref="UpdateMode"/> is used to decide which of Unity´s update loops
+    /// is called since some of the modes can create movement artifacts if different Unity versions.
+    /// <para/>
+    /// Note, this is just a script used for our example scenes and, therefore, not part of the actual API. We do not
+    /// guarantee that this script is working besides our examples.
+    /// </summary>
+    [AddComponentMenu("Polarith AI » Move » Package/Camera Follow")]
+    [RequireComponent(typeof(Camera))]
+    public sealed class CameraFollow : MonoBehaviour
+    {
+        #region Fields =================================================================================================
+
+        [Tooltip("Camera that shall follow the target.")]
+        [SerializeField]
+        private Camera cam;
+
+        [Tooltip("Target X-value of the camera´s rotation.")]
+        [SerializeField]
+        private float cameraAngle = 30;
+
+        [Tooltip("Affects how fast movement changes of the target are applied to the camera.")]
+        [SerializeField]
+        private float moveSpeed = 3;
+
+        [Tooltip("Distance between camera and target.")]
+        [SerializeField]
+        private float offset = 300;
+
+        [Tooltip("Target object the camera tries to follow.")]
+        [SerializeField]
+        private Transform target;
+
+        [Tooltip("Decides which parts of Unity´s update loops will be called.")]
+        [SerializeField]
+        private UpdateType updateMode = UpdateType.FixedUpdate;
+
+        #endregion // Fields
+
+        #region Enums ==================================================================================================
+
+        /// <summary>
+        /// Decides which parts of Unity´s update loops will be called.
+        /// </summary>
+        public enum UpdateType
+        {
+            /// <summary>
+            /// Controls Unity´s FixedUpdate function.
+            /// </summary>
+            FixedUpdate = 0,
+
+            /// <summary>
+            /// Controls Unity´s LateUpdate function.
+            /// </summary>
+            LateUpdate = 1,
+
+            /// <summary>
+            /// Controls Unity´s Update function.
+            /// </summary>
+            Update = 2
+        };
+
+        #endregion // Enums
+
+        #region Properties =============================================================================================
+
+        /// <summary>
+        /// Camera that shall follow the target.
+        /// </summary>
+        public Camera Camera
+        {
+            get { return cam; }
+            set { cam = value; }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Target X-value of the camera´s rotation.
+        /// </summary>
+        public float CameraAngle
+        {
+            get { return cameraAngle; }
+            set { cameraAngle = value; }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Affects how fast movement changes of the target are applied to the camera.
+        /// </summary>
+        public float MoveSpeed
+        {
+            get { return moveSpeed; }
+            set { moveSpeed = value; }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Distance between camera and target.
+        /// </summary>
+        public float Offset
+        {
+            get { return offset; }
+            set { offset = value; }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Target object the camera tries to follow.
+        /// </summary>
+        public Transform Target
+        {
+            get { return target; }
+            set { target = value; }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Decides which parts of Unity´s update loops will be called.
+        /// </summary>
+        public UpdateType UpdateMode
+        {
+            get { return updateMode; }
+            set { updateMode = value; }
+        }
+
+        #endregion // Properties
+
+        #region Methods ================================================================================================
+
+        private void FixedUpdate()
+        {
+            if (UpdateMode == UpdateType.FixedUpdate)
+                FollowTarget(Time.deltaTime);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void LateUpdate()
+        {
+            if (UpdateMode == UpdateType.LateUpdate)
+                FollowTarget(Time.deltaTime);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Update()
+        {
+            if (UpdateMode == UpdateType.Update)
+                FollowTarget(Time.deltaTime);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void FollowTarget(float deltaTime)
+        {
+            // if no target, or no time passed then we quit early, as there is nothing to do
+            if (!(deltaTime > 0) || Target == null)
+            {
+                return;
+            }
+
+            Vector3 goal = new Vector3(Target.position.x + (Target.position.x - Camera.transform.position.x),
+                                       Target.position.y,
+                                       Target.position.z);
+            goal -= Camera.transform.forward * Offset;
+
+            float dist = (Target.position - Camera.transform.position).magnitude;
+
+            // camera position moves towards target position:
+            Camera.transform.position = Vector3.Lerp(Camera.transform.position,
+                                                     goal,
+                                                     deltaTime * MoveSpeed * dist / Offset);
+            Camera.transform.rotation = Quaternion.Lerp(Camera.transform.rotation,
+                                                     Quaternion.Euler(CameraAngle,
+                                                                      Camera.transform.rotation.eulerAngles.y,
+                                                                      Camera.transform.rotation.eulerAngles.z),
+                                                     Time.deltaTime);
+        }
+
+        #endregion // Methods
+    } // class CameraFollow
+} // namespace Polarith.AI.Package
---
+++ Assets/Polarith/AI/Examples/Scripts/Controllers/RootMotionController.cs
@@ -0,0 +1,123 @@
+﻿using Polarith.AI.Move;
+using UnityEngine;
+
+namespace Polarith.AI.Package
+{
+    /// <summary>
+    /// This class illustrates how to combine Polarith.AI with Unitys state - and root motion mechanism. It acts as an
+    /// example and may be altered, improved or maybe used as base for more elaborate implemenations for specific
+    /// animation controllers. By combining Polarith AI and root motion mechanis different animation states like idle,
+    /// walk and run are easily manageble with just a few lines.
+    /// <para/>
+    /// This implementation does two things. First, the character is aligned along the direction received by <see
+    /// cref="AIMContext.DecidedDirection"/>. Second, a simple float parameter is passed to the <see cref="Animator"/>
+    /// to provide a hint how fast the character should move. This way, the different animation states, like idle or
+    /// walking are managed by the animation controller like it should be. Further information can be found here,
+    /// https://docs.unity3d.com/Manual/AnimationStateMachines.html. The actual movement of the character is done by the
+    /// root motion mechanis (https://docs.unity3d.com/Manual/RootMotion.html).
+    /// <para/>
+    /// A prerequisite is of course a fitting animation controller setup, like the SimpleWalkCycle.controller in the
+    /// Polarith AI package.
+    /// <para/>
+    /// Note, this is just a script for our example scenes and therefore not part of the actual API. We do not guarantee
+    /// that this script is working besides our examples.
+    /// </summary>
+    public class RootMotionController : MonoBehaviour
+    {
+        #region Fields =================================================================================================
+
+        /// <summary>
+        /// The animation controller that holds and manages the different animation states. This is a mandatory
+        /// reference since the decision of Polarith AI ( <see cref="Context"/>) is applied to this animation
+        /// controller. If the reference is <c>null</c>, the component searches for an attached Animator OnEnable.
+        /// </summary>
+        [Tooltip("The animation controller that holds and manages the different animation states. This is a " +
+            "mandatory reference since the decision of Polarith AI (see 'Context') is applied to this animation " +
+            "controller. If the reference is null, the component searches for an attached Animator OnEnable.")]
+        public Animator Animator;
+
+        /// <summary>
+        /// This component provides the results of the AI system. These results are then applied to the attached <see
+        /// cref="Animator"/>. Thus, the reference to an AIMContext component is mandatory. The controller is disabled
+        /// if no Context instance can be found at OnEnable.
+        /// </summary>
+        [Tooltip("This component provides the results of the AI system. These results are then applied to the " +
+            "attached Animator. Thus, the reference to an AIMContext component is mandatory.The controller is + " +
+            "disabled if no Context instance can be found at OnEnable.")]
+        public AIMContext Context;
+
+        /// <summary>
+        /// The maximum value of the parameter passed to the <see cref="AnimatorParameter"/> that is assumend to somehow
+        /// control the movement animation. Thus, it could be seen as a limit for movement speed.
+        /// </summary>
+        public float MovementSpeed = 0.5f;
+
+        /// <summary>
+        /// Controls how fast the character can rotate to a direction given by the <see cref="Context"/>. In radians per
+        /// second. For example, a value of 3.141 means that the character can turn around in one second.
+        /// </summary>
+        [Tooltip("Controls how fast the character can rotate to a direction given by the Context. In radians per " +
+            "second. For example, a value of 3.141 means that the character can turn around in one second.")]
+        public float RotationSpeed = 1.0f;
+
+        /// <summary>
+        /// If set equal to or greater than 0, the evaluated AI decision value is multiplied to the <see
+        /// cref="MovementSpeed"/>.
+        /// </summary>
+        [Tooltip("If set equal to or greater than 0, the evaluated AI decision value is multiplied to the 'Speed'.")]
+        [TargetObjective(true)]
+        public int ObjectiveAsSpeed = -1;
+
+        #endregion // Fields
+
+        #region Methods ================================================================================================
+
+        private void OnEnable()
+        {
+            if (Animator == null)
+                Animator = GetComponent<Animator>();
+
+            if (Context == null)
+                Context = GetComponent<AIMContext>();
+
+            if (Context == null || Animator == null)
+            {
+                Debug.LogWarning('(' + typeof(RootMotionController).Name + ") " + name +": deactivated because a " +
+                    "reference to either an AIMContext or an Animator is missing.");
+                enabled = false;
+                return;
+            }
+
+            Animator.applyRootMotion = true;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Update()
+        {
+            // Rotate the character
+            Vector3 targetDirection = Context.DecidedDirection;
+            float step = RotationSpeed * Time.deltaTime;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, step, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+
+            // Calculate rotation distance
+            float speedMultiplier = 1.0f;
+            if (Vector3.Angle(targetDirection, transform.forward) > 50.0f)
+                speedMultiplier = 0.0f;
+
+            // Move the character
+            if (ObjectiveAsSpeed >= 0 && ObjectiveAsSpeed < Context.DecidedValues.Count)
+            {
+                float magnitude = Context.DecidedValues[ObjectiveAsSpeed] * MovementSpeed;
+                magnitude = magnitude > MovementSpeed ? MovementSpeed : magnitude;
+                Animator.SetFloat("Speed", magnitude * speedMultiplier);
+            }
+            else
+            {
+                Animator.SetFloat("Speed", MovementSpeed * speedMultiplier);
+            }        
+        }
+        #endregion // Methods
+    } // class RootMotionController
+} // namespace Polarith.AI.Package
