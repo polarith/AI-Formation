2565d3f74808e042b5ed0689d37fa7e515374b81
Martin Zettwitz <martin.zettwitz@polarith.com>
2020-01-22 14:26:07 +0100

    updated lab ascene

---
+++ Assets/Polarith/AI/Examples/Scripts/TriggerFollowTarget.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Polarith.AI.Move;
+
+namespace Polarith.AI.Package
+{
+    /// <summary>
+    /// This script changes the target of a <see cref="AIMFollow"/> that enters the trigger.
+    /// Note, this is just a script for our example scenes and therefore not part of the actual API. We do not guarantee
+    /// that this script is working besides our examples.
+    /// </summary>
+    public class TriggerFollowTarget : MonoBehaviour
+    {
+        #region Fields =================================================================================================
+
+        /// <summary>
+        /// New target game object that is assigned to <see cref="AIMFollow.Target"/>.
+        /// </summary>
+        [Tooltip("New target GameObject of the FormationSetup.")]
+        public GameObject NewTargetObject;
+
+        #endregion // Fields
+
+        #region Methods ================================================================================================
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            AIMFollow f = other.transform.gameObject.GetComponent<AIMFollow>();
+            if (f != null)
+                f.Target = NewTargetObject;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            AIMFormationConfiguration c = collision.transform.gameObject.GetComponent<AIMFormationConfiguration>();
+            if (c != null)
+                c.UpdateConfig();                
+        }
+
+        #endregion // Methods
+    } // class TriggerFollowTarget
+} // namespace Polarith.AI.Package
